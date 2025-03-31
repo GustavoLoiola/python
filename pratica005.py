c365c7b5a00480baf00c50a4c16c0121dea1b663
GustavoLoiola <Gusantanaloiola@gmail.com>
2025-03-31 11:51:49 -0300

    mais um exercício de python

---
+++ pratica005.py
@@ -0,0 +1,7 @@
+a = float(input('Digite um número:'))
+b = float(input('Digite outro número:'))
+
+print('O resultado da soma é:', a+b)
+print('O resultado da divisão é:', a/b)
+print('O resultado da multipçicação é', a*b)
+print('O resultado da subtração é', a-b)
