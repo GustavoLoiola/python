cfdcd3b3d23ceb576f0e9c219e37033c2ea1e339
GustavoLoiola <Gusantanaloiola@gmail.com>
2025-04-01 15:25:42 -0300

    mais estudos em python

---
+++ pratica006.py
@@ -0,0 +1,10 @@
+nome = str(input('Qual é o seu nome?'))
+str(print('Prazer em te conhecer, {}'.format(nome)))
+str(print('Agora, iremos fazer uma conta de adição, {}'.format(nome)))
+
+n1 = int(input('Digite um valor:'))
+n2 = int(input('digite outro valor:'))
+soma = n1 + n2
+
+print('A soma de {0} e {1} é: {2}'.format (n1, n2, soma))
+str(print('Obrigado por usar o meu código!'))
