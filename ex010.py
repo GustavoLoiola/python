202d67efe854fcf54efc59f2c6098315c4788ac6
GustavoLoiola <Gusantanaloiola@gmail.com>
2025-04-02 14:05:17 -0300

    exercício conversor de moedas em python

---
+++ ex010.py
@@ -0,0 +1,2 @@
+r = float(input('Digite um valor em Real para saber a conversão para Dólar e Euro: R$'))
+print('O valor {:.2f} em Reais equivale atualmente (02/04/2025) em {:.2f} Dólares e {:.2f} Euros.'.format(r, (r/5.70), (r/6.20)))
