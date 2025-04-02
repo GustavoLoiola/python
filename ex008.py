6faee876c1c21ffe2bdec2712847b00160cd366f
GustavoLoiola <Gusantanaloiola@gmail.com>
2025-04-02 13:52:24 -0300

    ex taboada em python

--- ex008.py
+++ ex008.py
@@ -1,2 +1,2 @@
 n1 = float(input('Digite um número em Metros:'))
-print('O valor digitado corresponde a {:.0f} em milímetros e {:.0f} em centímetros.'.format(n1*1000, n1*100))
+print('O valor digitado corresponde a {:.0f} milímetros, {:.0f} centímetros, {:.0f} hectômetros e {:.0f} quilometros'.format(n1*1000, n1*100, n1/100, n1/1000))
