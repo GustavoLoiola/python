6c5c0b3152df15d7a9c838508de414ced14829c0
GustavoLoiola <Gusantanaloiola@gmail.com>
2025-04-09 15:11:22 -0300

    ex017

---
+++ ex017.py
@@ -0,0 +1,6 @@
+import math
+
+co = float(input('Digite o valor do cateto oposto: '))
+ca = float(input('Digite o valor do cateto adjacente: '))
+hi = math.hypot(co, ca)
+print('A Hipotenusa vale: {:.2f}'.format(hi))
